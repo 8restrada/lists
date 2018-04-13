f63bdd74f2658fd6c25a4a6e891ae4104e55e83a
8restrada <31480941+8restrada@users.noreply.github.com>
2018-04-13 09:18:30 -0500

    Add files via upload

---
+++ listTasks.js
@@ -0,0 +1,109 @@
+
+function largest(list){
+let largest = list[0];
+    for(let a = 0; a < list.length; a++){
+        if(list[a] > largest){
+            largest = list[a];
+        }
+    }
+    return largest;
+}
+
+function reverse(list){
+let array = [];
+    for(let a = 0; a < list.length; a++){
+        array.push(list[a]);
+    }
+    return array;
+}
+
+//Task 3
+function cotains(list,e){
+    for(let c=0; c<=list.length; c++){
+        if(list[c]==e){
+
+        }
+    }
+    if true(return e){
+        else(return "this number isnt found")
+    }
+}
+
+//Task 4
+function oddElements(list){
+let array = []
+for(let a=0; a<list.length; a=a+2){
+    odd.push(list[a]);
+}
+return array;
+}
+
+
+//Task 5
+function total(list){
+let running = [];
+for(let a=0; a<=list.length; a++){
+    running=running+list[a];
+}
+return running;
+}
+//Task 6
+function isPalindrome(list){
+for(let a=0; a<list.length; a++){
+}
+let race = list.reverse(list);
+for(let b=0; b<list.length; b++){
+    list[a]==list[b];
+}
+if true('Polindrome Affirmative');
+else('Polindrome Negative');
+}
+
+//Task 7
+//Do second to last
+function recursiveTotal(list){
+let recursiveTotal = function(list){
+    if(list.length == 1){
+        return list[0];
+    }
+else(){
+    return total(list);
+}
+}
+}
+//Task 8
+function concatenate(listA, listB){
+let listX=[];
+for(let a=0; a<listA.length; a++){
+    listX.push(listA);
+}
+let listY=[];
+for(let b=0; b<listB.length; a++){
+    listY.push(listB);
+}
+return reverse(listC)
+}
+
+//Task 9
+function weave(listA, listB){
+
+}
+
+//Task 10
+//Attempt last
+function mergeInOrder(listA, listB){
+
+}
+
+//Task 11
+function rotate(list,k){
+
+}
+
+//Task 12
+function fibonacci(){
+
+}
+
+console.log(largest([3,2,4,5,6,7,8,9,10,1]));
+console.log(reverse([3,2,4,5,6,7,8,9,10,1]));
